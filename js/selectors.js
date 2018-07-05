7f3e4369c2c2398a0053f4d0b6940582c1633dbf
Felix Chan <felixchan29@gmail.com>
2018-07-05 16:18:25 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,5 +17,5 @@ function divSelector(){
 
 function firstListItem(){
   console.log($('ul#pic-list li:first-child'));
-  return $('ul#pic-list img:first-child');
+  return $('ul#pic-list li:first-child');
 }
